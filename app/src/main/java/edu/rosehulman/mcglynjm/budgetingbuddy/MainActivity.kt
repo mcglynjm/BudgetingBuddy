51375d36345662a011ad0db7041f0dba319b1de5
mcglynjm <62721979+mcglynjm@users.noreply.github.com>
2021-02-02 19:28:21 -0500

    Added AuthUI stuff

--- app/src/main/java/edu/rosehulman/mcglynjm/budgetingbuddy/MainActivity.kt
+++ app/src/main/java/edu/rosehulman/mcglynjm/budgetingbuddy/MainActivity.kt
@@ -5,13 +5,21 @@ import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import androidx.fragment.app.Fragment
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
 
-class MainActivity : AppCompatActivity(), FragmentViewer, TransactionSelect {
+class MainActivity : AppCompatActivity(), FragmentViewer, TransactionSelect, SplashFragment.OnLoginButtonPressedListener {
+    private val auth = FirebaseAuth.getInstance()
+    lateinit var authStateListener : FirebaseAuth.AuthStateListener
+    // Request code for launching the sign in Intent.
+    private val RC_SIGN_IN = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //setSupportActionBar(findViewById(R.id.toolbar))
-        viewFragment(HomeFragment(), getString(R.string.home))
+        //viewFragment(HomeFragment(), getString(R.string.home))
+        initializeListeners()
     }
 
 
@@ -53,6 +61,62 @@ class MainActivity : AppCompatActivity(), FragmentViewer, TransactionSelect {
             Log.d(Constants.TAG, "Making $type fragment")
         }
     }
+    override fun onStart() {
+        super.onStart()
+        auth.addAuthStateListener(authStateListener)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        auth.removeAuthStateListener(authStateListener)
+    }
+    private fun initializeListeners() {
+        // TODO: Create an AuthStateListener that passes the UID
+        // to the MovieQuoteFragment if the user is logged in
+        // and goes back to the Splash fragment otherwise.
+        // See https://firebase.google.com/docs/auth/users#the_user_lifecycle
+        authStateListener = FirebaseAuth.AuthStateListener {
+            val user = auth.currentUser
+            Log.d(Constants.TAG, "In auth listener, User: $user")
+            if (user != null) {
+                Log.d(Constants.TAG, "UID: ${user.uid}")
+                Log.d(Constants.TAG, "Name: ${user.displayName}")
+                // plus email, photoUrl, phoneNumber
+                viewFragment(HomeFragment(), getString(R.string.home))
+            } else {
+                viewFragment(SplashFragment(), "splash")
+            }
+        }
+    }
+
+    private fun switchToSplashFragment() {
+        viewFragment(SplashFragment(), "splash")
+    }
+
+
+    override fun onLoginButtonPressed() {
+        launchLoginUI()
+    }
+
+    private fun launchLoginUI() {
+        // TODO: Build a login intent and startActivityForResult(intent, ...)
+        // For details, see https://firebase.google.com/docs/auth/android/firebaseui#sign_in
+// Choose authentication providers
+        val providers = arrayListOf(
+                AuthUI.IdpConfig.EmailBuilder().build(),
+                AuthUI.IdpConfig.PhoneBuilder().build(),
+                AuthUI.IdpConfig.GoogleBuilder().build())
+
+// Create and launch sign-in intent
+        val loginIntent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                //.setLogo(R.drawable.ic_launcher_custom)
+                .build()
+
+        startActivityForResult(loginIntent, RC_SIGN_IN)
+    }
+
 
     override fun onTransactionSelected(transaction: ManualTransaction) {
         TODO("Not yet implemented")
